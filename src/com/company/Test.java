e31d19993b9652896c979f351e1a609eea52f800
Gigotaur <90615070+Gigotaur@users.noreply.github.com>
2022-01-18 15:15:06 +0300

    Update Test.java

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,25 +3,25 @@ package com.company;
 public class Test {
     public boolean testProgram() {
         if (picture.getColor(-2, -2) != SimpleColor.GRAY) {
-            System.out.println("Test 1 is not completed");
+            System.out.println("Gray-Test is not completed");
             return false;
         } else if (picture.getColor(-6, 0) != SimpleColor.BLUE) {
-            System.out.println("Test 2 is not completed");
+            System.out.println("Blue-Test_1 is not completed");
             return false;
         } else if (picture.getColor(-3, 2) != SimpleColor.BLUE) {
-            System.out.println("Test 3 is not completed");
+            System.out.println("Blue-Test_2 is not completed");
             return false;
         } else if (picture.getColor(-6, 1) != SimpleColor.WHITE) {
-            System.out.println("Test 4 is not completed");
+            System.out.println("White-Test_1 is not completed");
             return false;
         } else if (picture.getColor(2, 3) != SimpleColor.WHITE) {
-            System.out.println("Test 5 is not completed");
+            System.out.println("White-Test_2 is not completed");
             return false;
         } else if (picture.getColor(-1, 2) != SimpleColor.GREEN) {
-            System.out.println("Test 6 is not completed");
+            System.out.println("Green-Test is not completed");
             return false;
         } else if (picture.getColor(1, -1) != SimpleColor.YELLOW) {
-            System.out.println("Test 7 is not completed");
+            System.out.println("yellow-Test is not completed");
             return false;
         } else {
             return true;
